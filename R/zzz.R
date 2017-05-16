37cb19400ba83cc6993cad69f018777a11af5709
fdetsch <florian.detsch@staff.uni-marburg.de>
2017-05-16 09:41:24 +0200

    Update: installation of 'leaflet.extras' and 'mapedit' .onLoad().

--- R/zzz.R
+++ R/zzz.R
@@ -1,14 +1,27 @@
-#.onLoad <- function(lib, pkg)
-#{
-#    win <- options("warn")
-#    options(warn=-1)
-#    MODISoptions(save=FALSE, checkPackages=FALSE, quiet=TRUE)
-#    options(warn=win$warn)
-#}
-.onAttach <- function(lib, pkg)
-{
-    #win <- options("warn")
-    #options(warn=-1)
-    packageStartupMessage(MODISoptions(save=FALSE, checkTools=FALSE, quiet=TRUE))
+.onLoad <- function(lib, pkg) {
+  
+  ## leaflet.extras
+  if (!"leaflet.extras" %in% utils::installed.packages() |
+      utils::packageVersion("leaflet.extras") < '0.1.9009') {
+    if ("leaflet.extras" %in% loadedNamespaces()) {
+      detach("package:leaflet.extras", unload = TRUE)
+    }
+    
+    devtools::install_github("bhaskarvk/leaflet.extras")
+  }
+  
+  ## mapedit
+  if (!"mapedit" %in% utils::installed.packages() |
+      utils::packageVersion("mapedit") < '0.0.2') {
+    if ("mapedit" %in% loadedNamespaces()) {
+      detach("package:mapedit", unload = TRUE)
+    }
+    
+    devtools::install_github("r-spatial/mapedit")
+  }
+}
+
+.onAttach <- function(lib, pkg) {
+  packageStartupMessage(MODISoptions(save=FALSE, checkTools=FALSE, quiet=TRUE))
 }
 

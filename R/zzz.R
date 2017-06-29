b34d783f709cfc2f9fa8683f4303635ecee049b8
fdetsch <florian.detsch@staff.uni-marburg.de>
2017-06-29 10:30:29 +0200

    Update: temporarily moved now-on-CRAN leaflet.extras, which is required by mapedit, to Depends.

--- R/zzz.R
+++ R/zzz.R
@@ -1,17 +1,5 @@
 .onLoad <- function(lib, pkg) {
   
-  ## leaflet.extras
-  avl <- length(find.package("leaflet.extras", quiet = TRUE)) > 0
-  vld <- ifelse(avl, utils::packageVersion("leaflet.extras") >= '0.1.9009', FALSE)
-
-  if (any(!avl, !vld)) {
-    if ("leaflet.extras" %in% loadedNamespaces()) {
-      detach("package:leaflet.extras", unload = TRUE)
-    }
-    
-    devtools::install_github("bhaskarvk/leaflet.extras")
-  }
-  
   ## mapedit
   avl <- length(find.package("mapedit", quiet = TRUE)) > 0
   vld <- ifelse(avl, utils::packageVersion("mapedit") >= '0.0.2', FALSE)
@@ -26,6 +14,8 @@
 }
 
 .onAttach <- function(lib, pkg) {
-  packageStartupMessage(MODISoptions(save=FALSE, checkTools=FALSE, quiet=TRUE))
+  packageStartupMessage(
+    x <- capture.output(MODISoptions(save=FALSE, checkTools=FALSE, quiet=TRUE))
+  )
 }
 

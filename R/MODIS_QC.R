c213857ef1eaceaa6f5cd2f90992a067b6331e54
MatMatt <matteo@mattiuzzi.com>
2018-07-12 16:06:51 +0200

    added several M.D15A2H and M.D15A3H products

--- R/MODIS_QC.R
+++ R/MODIS_QC.R
@@ -5,60 +5,60 @@
 ########### M.D09
 
 MOD09A1_QC <- data.frame(
-        LongName=c("MODLAND_QA", "band 1 data quality four bit range", "band 2 data quality four bit range", "band 3 data quality four bit range", "band 4 data quality four bit range", "band 5 data quality four bit range", "band 6 data quality four bit range", "band 7 data quality four bit range", "atmospheric correction performed", "adjacency correction performed"), 
-        bitShift=c(0,2,6,10,14,18,22,26,30,31),
-        bitMask=c(3,15,15,15,15,15,15,15,1,1)
-    )
-    
+  LongName=c("MODLAND_QA", "band 1 data quality four bit range", "band 2 data quality four bit range", "band 3 data quality four bit range", "band 4 data quality four bit range", "band 5 data quality four bit range", "band 6 data quality four bit range", "band 7 data quality four bit range", "atmospheric correction performed", "adjacency correction performed"), 
+  bitShift=c(0,2,6,10,14,18,22,26,30,31),
+  bitMask=c(3,15,15,15,15,15,15,15,1,1)
+)
+
 MYD09A1_QC <- data.frame(
-        LongName=c("MODLAND_QA", "band 1 data quality four bit range", "band 2 data quality four bit range", "band 3 data quality four bit range", "band 4 data quality four bit range", "band 5 data quality four bit range", "band 6 data quality four bit range", "band 7 data quality four bit range", "atmospheric correction performed", "adjacency correction performed"), 
-        bitShift=c(0,2,6,10,14,18,22,26,30,31),
-        bitMask=c(3,15,15,15,15,15,15,15,1,1)
-    )
+  LongName=c("MODLAND_QA", "band 1 data quality four bit range", "band 2 data quality four bit range", "band 3 data quality four bit range", "band 4 data quality four bit range", "band 5 data quality four bit range", "band 6 data quality four bit range", "band 7 data quality four bit range", "atmospheric correction performed", "adjacency correction performed"), 
+  bitShift=c(0,2,6,10,14,18,22,26,30,31),
+  bitMask=c(3,15,15,15,15,15,15,15,1,1)
+)
 
 MOD09GQ_QC <- data.frame(
-        LongName=c("MODLAND_QA", "cloud state", "band 1 data quality four bit range", "band 2 data quality four bit range",
-        "atmospheric correction performed", "adjacency correction performed", "spare (unused)"), 
-        bitShift=c(0,2,4,8,12,13,14),
-        bitMask=c(3,3,15,15,1,1,3)
-    )
+  LongName=c("MODLAND_QA", "cloud state", "band 1 data quality four bit range", "band 2 data quality four bit range",
+             "atmospheric correction performed", "adjacency correction performed", "spare (unused)"), 
+  bitShift=c(0,2,4,8,12,13,14),
+  bitMask=c(3,3,15,15,1,1,3)
+)
 
 MYD09GQ_QC <- data.frame(
-        LongName=c("MODLAND_QA", "cloud state", "band 1 data quality four bit range", "band 2 data quality four bit range",
-        "atmospheric correction performed", "adjacency correction performed", "spare (unused)"), 
-        bitShift=c(0,2,4,8,12,13,14),
-        bitMask=c(3,3,15,15,1,1,3)
-    )
+  LongName=c("MODLAND_QA", "cloud state", "band 1 data quality four bit range", "band 2 data quality four bit range",
+             "atmospheric correction performed", "adjacency correction performed", "spare (unused)"), 
+  bitShift=c(0,2,4,8,12,13,14),
+  bitMask=c(3,3,15,15,1,1,3)
+)
 
 MOD09GA_QC <- data.frame(
-        LongName=c("cloud state", "cloud shadow", "land/water flag", "aerosol quantity", "cirrus detected",
-        "internal cloud algorithm flag", "internal fire algorithm flag", "MOD35 snow/ice flag",
-        "Pixel is adjacent to cloud", "BRDF correction performed", "internal snow mask"), 
-        bitShift=c(0,2,3,6,8,10,11,12,13,14,15),
-        bitMask=c(3,1,7,3,3,1,1,1,1,1,1)
-    )
+  LongName=c("cloud state", "cloud shadow", "land/water flag", "aerosol quantity", "cirrus detected",
+             "internal cloud algorithm flag", "internal fire algorithm flag", "MOD35 snow/ice flag",
+             "Pixel is adjacent to cloud", "BRDF correction performed", "internal snow mask"), 
+  bitShift=c(0,2,3,6,8,10,11,12,13,14,15),
+  bitMask=c(3,1,7,3,3,1,1,1,1,1,1)
+)
 
 MYD09GA_QC <- data.frame(
-        LongName=c("cloud state", "cloud shadow", "land/water flag", "aerosol quantity", "cirrus detected",
-        "internal cloud algorithm flag", "internal fire algorithm flag", "MOD35 snow/ice flag",
-        "Pixel is adjacent to cloud", "BRDF correction performed", "internal snow mask"), 
-        bitShift=c(0,2,3,6,8,10,11,12,13,14,15),
-        bitMask=c(3,1,7,3,3,1,1,1,1,1,1)
-    )
+  LongName=c("cloud state", "cloud shadow", "land/water flag", "aerosol quantity", "cirrus detected",
+             "internal cloud algorithm flag", "internal fire algorithm flag", "MOD35 snow/ice flag",
+             "Pixel is adjacent to cloud", "BRDF correction performed", "internal snow mask"), 
+  bitShift=c(0,2,3,6,8,10,11,12,13,14,15),
+  bitMask=c(3,1,7,3,3,1,1,1,1,1,1)
+)
 
 MOD09Q1_QC <- data.frame(
-        LongName=c("MODLAND_QA", "cloud state", "band 1 data quality four bit range", "band 2 data quality four bit range",
-        "atmospheric correction performed", "adjacency correction performed", "different orbit from 500 m", "spare (unused)"), 
-        bitShift=c(0,2,4,8,12,13,14,15),
-        bitMask=c(3,3,15,15,1,1,1,1)
-    )
+  LongName=c("MODLAND_QA", "cloud state", "band 1 data quality four bit range", "band 2 data quality four bit range",
+             "atmospheric correction performed", "adjacency correction performed", "different orbit from 500 m", "spare (unused)"), 
+  bitShift=c(0,2,4,8,12,13,14,15),
+  bitMask=c(3,3,15,15,1,1,1,1)
+)
 
 MYD09Q1_QC <- data.frame(
-        LongName=c("MODLAND_QA", "cloud state", "band 1 data quality four bit range", "band 2 data quality four bit range",
-        "atmospheric correction performed", "adjacency correction performed", "different orbit from 500 m", "spare (unused)"), 
-        bitShift=c(0,2,4,8,12,13,14,15),
-        bitMask=c(3,3,15,15,1,1,1,1)
-    )
+  LongName=c("MODLAND_QA", "cloud state", "band 1 data quality four bit range", "band 2 data quality four bit range",
+             "atmospheric correction performed", "adjacency correction performed", "different orbit from 500 m", "spare (unused)"), 
+  bitShift=c(0,2,4,8,12,13,14,15),
+  bitMask=c(3,3,15,15,1,1,1,1)
+)
 
 ########### M.D11
 
@@ -100,16 +100,16 @@ MYD11B1_QC <- data.frame(
 )
 
 MOD11C1_QC <- data.frame(
-        LongName=c("Mandatory QA flag", "Data quality flag", "Emissivity error flag", "LST error flag"), 
-        bitShift=c(0,2,4,6),
-        bitMask=c(3,3,3,3)
-    )
+  LongName=c("Mandatory QA flag", "Data quality flag", "Emissivity error flag", "LST error flag"), 
+  bitShift=c(0,2,4,6),
+  bitMask=c(3,3,3,3)
+)
 
 MYD11C1_QC <- data.frame(
-        LongName=c("Mandatory QA flag", "Data quality flag", "Emissivity error flag", "LST error flag"), 
-        bitShift=c(0,2,4,6),
-        bitMask=c(3,3,3,3)
-    )
+  LongName=c("Mandatory QA flag", "Data quality flag", "Emissivity error flag", "LST error flag"), 
+  bitShift=c(0,2,4,6),
+  bitMask=c(3,3,3,3)
+)
 
 MOD11C2_QC <- data.frame(
   LongName=c("Mandatory QA flag", "Data quality flag", "Emissivity error flag", "LST error flag"), 
@@ -146,70 +146,105 @@ MCD12C1_QC <- data.frame(
 ############ M.D13
 
 MOD13Q1_QC <- data.frame(
-        LongName=c("MODLAND_QA", "VI usefulness", "Aerosol quantity", "Adjacent cloud detected",
-        "Atmosphere BRDF correction performed", "Mixed Clouds", "Land/Water Flag", "Possible snow/ice", "Possible shadow"), 
-        bitShift=c(0,2,6,8,9,10,11,14,15),
-        bitMask=c(3,15,3,1,1,1,7,1,1)
-    )
+  LongName=c("MODLAND_QA", "VI usefulness", "Aerosol quantity", "Adjacent cloud detected",
+             "Atmosphere BRDF correction performed", "Mixed Clouds", "Land/Water Flag", "Possible snow/ice", "Possible shadow"), 
+  bitShift=c(0,2,6,8,9,10,11,14,15),
+  bitMask=c(3,15,3,1,1,1,7,1,1)
+)
 
 MYD13Q1_QC <- data.frame(
-        LongName=c("MODLAND_QA", "VI usefulness", "Aerosol quantity", "Adjacent cloud detected",
-        "Atmosphere BRDF correction performed", "Mixed Clouds", "Land/Water Flag", "Possible snow/ice", "Possible shadow"), 
-        bitShift=c(0,2,6,8,9,10,11,14,15),
-        bitMask=c(3,15,3,1,1,1,7,1,1)
-    )
+  LongName=c("MODLAND_QA", "VI usefulness", "Aerosol quantity", "Adjacent cloud detected",
+             "Atmosphere BRDF correction performed", "Mixed Clouds", "Land/Water Flag", "Possible snow/ice", "Possible shadow"), 
+  bitShift=c(0,2,6,8,9,10,11,14,15),
+  bitMask=c(3,15,3,1,1,1,7,1,1)
+)
 
 MYD13C1_QC <- data.frame(
-        LongName=c("MODLAND_QA", "VI usefulness", "Aerosol quantity", "Adjacent cloud detected",
-        "Atmosphere BRDF correction performed", "Mixed Clouds", "Land/Water Flag", "Geospatial quality"), 
-        bitShift=c(0,2,6,8,9,10,11,14),
-        bitMask=c(3,15,3,1,1,1,7,3)
-    )
+  LongName=c("MODLAND_QA", "VI usefulness", "Aerosol quantity", "Adjacent cloud detected",
+             "Atmosphere BRDF correction performed", "Mixed Clouds", "Land/Water Flag", "Geospatial quality"), 
+  bitShift=c(0,2,6,8,9,10,11,14),
+  bitMask=c(3,15,3,1,1,1,7,3)
+)
 
 MOD13C1_QC <- data.frame(
-        LongName=c("MODLAND_QA", "VI usefulness", "Aerosol quantity", "Adjacent cloud detected",
-        "Atmosphere BRDF correction performed", "Mixed Clouds", "Land/Water Flag", "Geospatial quality"), 
-        bitShift=c(0,2,6,8,9,10,11,14),
-        bitMask=c(3,15,3,1,1,1,7,3)
-    )
+  LongName=c("MODLAND_QA", "VI usefulness", "Aerosol quantity", "Adjacent cloud detected",
+             "Atmosphere BRDF correction performed", "Mixed Clouds", "Land/Water Flag", "Geospatial quality"), 
+  bitShift=c(0,2,6,8,9,10,11,14),
+  bitMask=c(3,15,3,1,1,1,7,3)
+)
 
 MYD13A2_QC <- data.frame(
-        LongName=c("MODLAND_QA", "VI usefulness", "Aerosol quantity", "Adjacent cloud detected",
-        "Atmosphere BRDF correction performed", "Mixed Clouds", "Land/Water Flag", "Possible snow/ice", "Possible shadow"), 
-        bitShift=c(0,2,6,8,9,10,11,14,15),
-        bitMask=c(3,15,3,1,1,1,7,1,1)
-    )
+  LongName=c("MODLAND_QA", "VI usefulness", "Aerosol quantity", "Adjacent cloud detected",
+             "Atmosphere BRDF correction performed", "Mixed Clouds", "Land/Water Flag", "Possible snow/ice", "Possible shadow"), 
+  bitShift=c(0,2,6,8,9,10,11,14,15),
+  bitMask=c(3,15,3,1,1,1,7,1,1)
+)
 
 MOD13A2_QC <- data.frame(
-        LongName=c("MODLAND_QA", "VI usefulness", "Aerosol quantity", "Adjacent cloud detected",
-        "Atmosphere BRDF correction performed", "Mixed Clouds", "Land/Water Flag", "Possible snow/ice", "Possible shadow"), 
-        bitShift=c(0,2,6,8,9,10,11,14,15),
-        bitMask=c(3,15,3,1,1,1,7,1,1)
-    )
+  LongName=c("MODLAND_QA", "VI usefulness", "Aerosol quantity", "Adjacent cloud detected",
+             "Atmosphere BRDF correction performed", "Mixed Clouds", "Land/Water Flag", "Possible snow/ice", "Possible shadow"), 
+  bitShift=c(0,2,6,8,9,10,11,14,15),
+  bitMask=c(3,15,3,1,1,1,7,1,1)
+)
 
 
 MYD13A3_QC <- data.frame(
-        LongName=c("MODLAND_QA", "VI usefulness", "Aerosol quantity", "Adjacent cloud detected",
-        "Atmosphere BRDF correction performed", "Mixed Clouds", "Land/Water Flag", "Possible snow/ice", "Possible shadow"), 
-        bitShift=c(0,2,6,8,9,10,11,14,15),
-        bitMask=c(3,15,3,1,1,1,7,1,1)
-    )
+  LongName=c("MODLAND_QA", "VI usefulness", "Aerosol quantity", "Adjacent cloud detected",
+             "Atmosphere BRDF correction performed", "Mixed Clouds", "Land/Water Flag", "Possible snow/ice", "Possible shadow"), 
+  bitShift=c(0,2,6,8,9,10,11,14,15),
+  bitMask=c(3,15,3,1,1,1,7,1,1)
+)
 
 MOD13A3_QC <- data.frame(
-        LongName=c("MODLAND_QA", "VI usefulness", "Aerosol quantity", "Adjacent cloud detected",
-        "Atmosphere BRDF correction performed", "Mixed Clouds", "Land/Water Flag", "Possible snow/ice", "Possible shadow"), 
-        bitShift=c(0,2,6,8,9,10,11,14,15),
-        bitMask=c(3,15,3,1,1,1,7,1,1)
-    )
+  LongName=c("MODLAND_QA", "VI usefulness", "Aerosol quantity", "Adjacent cloud detected",
+             "Atmosphere BRDF correction performed", "Mixed Clouds", "Land/Water Flag", "Possible snow/ice", "Possible shadow"), 
+  bitShift=c(0,2,6,8,9,10,11,14,15),
+  bitMask=c(3,15,3,1,1,1,7,1,1)
+)
 
-#### MCD15
+#### M.D15
 
-MCD15A2_QC <- data.frame(
+MCD15A2H_QC <- data.frame(
   LongName=c("MODLAND_QC bits", "Sensor", "DeadDetector", "CloudState (inherited from Aggregate_QC bits {0,1} cloud state)",
              "SCF_QC (five level confidence score)"), 
   bitShift=c(0,1,2,3,5),
   bitMask=c(1,1,1,3,7)
-  )
+)
+
+MOD15A2H_QC <- data.frame(
+  LongName=c("MODLAND_QC bits", "Sensor", "DeadDetector", "CloudState (inherited from Aggregate_QC bits {0,1} cloud state)",
+             "SCF_QC (five level confidence score)"), 
+  bitShift=c(0,1,2,3,5),
+  bitMask=c(1,1,1,3,7)
+)
+
+MYD15A2H_QC <- data.frame(
+  LongName=c("MODLAND_QC bits", "Sensor", "DeadDetector", "CloudState (inherited from Aggregate_QC bits {0,1} cloud state)",
+             "SCF_QC (five level confidence score)"), 
+  bitShift=c(0,1,2,3,5),
+  bitMask=c(1,1,1,3,7)
+)
+
+MCD15A3H_QC <- data.frame(
+  LongName=c("MODLAND_QC bits", "Sensor", "DeadDetector", "CloudState (inherited from Aggregate_QC bits {0,1} cloud state)",
+             "SCF_QC (five level confidence score)"), 
+  bitShift=c(0,1,2,3,5),
+  bitMask=c(1,1,1,3,7)
+)
+
+MOD15A3H_QC <- data.frame(
+  LongName=c("MODLAND_QC bits", "Sensor", "DeadDetector", "CloudState (inherited from Aggregate_QC bits {0,1} cloud state)",
+             "SCF_QC (five level confidence score)"), 
+  bitShift=c(0,1,2,3,5),
+  bitMask=c(1,1,1,3,7)
+)
+
+MYD15A3H_QC <- data.frame(
+  LongName=c("MODLAND_QC bits", "Sensor", "DeadDetector", "CloudState (inherited from Aggregate_QC bits {0,1} cloud state)",
+             "SCF_QC (five level confidence score)"), 
+  bitShift=c(0,1,2,3,5),
+  bitMask=c(1,1,1,3,7)
+)
 
 #### M.D17
 
@@ -218,11 +253,11 @@ MOD17A2_QC <- data.frame(
              "SCF_QC (five level confidence score)"),
   bitShift=c(0,1,2,3,5),
   bitMask=c(1,1,1,3,7)
-  )
+)
 
 MYD17A2_QC <- data.frame(
-    LongName=c("MODLAND_QC bits", "Sensor", "DeadDetector", "CloudState (inherited from Aggregate_QC bits {0,1} cloud state)",
-               "SCF_QC (five level confidence score)"),
-    bitShift=c(0,1,2,3,5),
-    bitMask=c(1,1,1,3,7)
-    )
+  LongName=c("MODLAND_QC bits", "Sensor", "DeadDetector", "CloudState (inherited from Aggregate_QC bits {0,1} cloud state)",
+             "SCF_QC (five level confidence score)"),
+  bitShift=c(0,1,2,3,5),
+  bitMask=c(1,1,1,3,7)
+)

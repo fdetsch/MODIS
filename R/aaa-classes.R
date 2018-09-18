bfa806cad66c762fd591e49de9c9dda2ff385625
fdetsch <fdetsch@web.de>
2018-09-18 16:21:33 +0200

    Initial upload: 'MODISproduct' class.

--- R/aaa-classes.R
+++ R/aaa-classes.R
@@ -25,3 +25,43 @@ setClass('MODISextent',
                    system = 'character',
                    target = 'listORnull')
          )
+
+NULL
+
+#' Class MODISproduct
+#'
+#' @description An object of class \code{MODISproduct}, typically created as the 
+#' \code{\link{invisible}} output of \code{\link[MODIS]{getProduct}}.
+#'
+#' @slot request User request as \code{character}.
+#' @slot PF1,PF2,PF3,PF4 Platform specific path feature for LP DAAC, LAADS, NTSG 
+#' and NSIDC as \code{character}.
+#' @slot PD Product specific code number as 2-digit \code{character}.
+#' @slot PLATFORM Satellite platform on which MODIS sensor is mounted; one of 
+#' \code{c("Terra", "Aqua")}.
+#' @slot TYPE Product type; one of \code{c("Tile", "CMG", "Swath")}.
+#' @slot PRODUCT MODIS product identified from 'request' as \code{character}.
+#' @slot SOURCE Product specific MODIS download server(s) as \code{list}.
+#' @slot CCC Product specific MODIS data collection as 3-digit \code{character}.
+#'
+#' @exportClass MODISproduct
+#' @name MODISproduct-class
+NULL
+
+setClassUnion("charORnull", c("character", "NULL"))
+
+setClass('MODISproduct',
+         slots = c(request = 'character', 
+                   PF1 = 'character', 
+                   PF2 = 'character', 
+                   PF3 = 'character', 
+                   PF4 = 'character', 
+                   PD = 'character',
+                   PLATFORM = 'character', 
+                   TYPE = 'character', 
+                   PRODUCT = 'character', 
+                   SOURCE = 'list', 
+                   CCC = 'charORnull')
+         )
+
+NULL
